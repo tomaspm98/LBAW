30c7a610038d767eed54acdaf5c67d22d4648b86
ricardo-peralta-0000 <ricardoperalta0000@gmail.com>
2023-10-17 23:31:17 +0100

    Full working create and populate

--- database/create.sql
+++ database/create.sql
@@ -1,3 +1,30 @@
+------------------------------------------------------------------------------------------------------------------------
+-- Project: Q&A Website
+-- Course: LBAW 2023/2024
+-- Group: 11
+------------------------------------------------------------------------------------------------------------------------
+-- Authors:
+-- António José Salazar Correia, up201804832@up.pt
+-- Gonçalo Nuno Leitão Pinho da Costa, up202103336@up.pt
+-- Tomás Pereira Maciel, up202006845@up.pt
+-- Ricardo Miguel Matos Oliveira Peralta, up2206392@up.pt
+------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------
+-- SCHEMA FOR LBAW2311
+------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------
+
+
+
+------------------------------------------------------------------------------------------------------------------------
+-- DROP ALL FROM LBAW2311 SCHEMA
+------------------------------------------------------------------------------------------------------------------------
 -- DROP TABLES FROM LBAW2311 SCHEMA
 DROP TABLE IF EXISTS admin;
 DROP TABLE IF EXISTS moderator CASCADE;
@@ -10,27 +37,30 @@ DROP TABLE IF EXISTS notification;
 DROP TABLE IF EXISTS answer CASCADE;
 DROP TABLE IF EXISTS question CASCADE;
 DROP TABLE IF EXISTS comment CASCADE;
---DROP TABLE IF EXISTS content CASCADE;
+DROP TABLE IF EXISTS content CASCADE;
 DROP TABLE IF EXISTS member CASCADE;
 DROP TABLE IF EXISTS badge;
 DROP TABLE IF EXISTS tag;
 DROP TABLE IF EXISTS vote;
 DROP TABLE IF EXISTS report;
 DROP TABLE IF EXISTS userfollowquestion;
-
 -- DROP TYPES FROM LBAW2311 SCHEMA
 DROP TYPE IF EXISTS voteType;
 DROP TYPE IF EXISTS entityType;
 DROP TYPE IF EXISTS reportReasonType;
 DROP TYPE IF EXISTS notificationType;
 
+------------------------------------------------------------------------------------------------------------------------
 -- CREATE TYPES/DOMAINS FOR LBAW2311 SCHEMA
+------------------------------------------------------------------------------------------------------------------------
 CREATE TYPE voteType AS ENUM ('up', 'down', 'out');
 CREATE TYPE entityType AS ENUM ('question', 'answer', 'comment');
 CREATE TYPE reportReasonType AS ENUM ('spam', 'offensive', 'Rules Violation', 'Inappropriate tag');
 CREATE TYPE notificationType AS ENUM ('question', 'answer', 'comment', 'badge');
 
-
+------------------------------------------------------------------------------------------------------------------------
+-- CREATE TABLES FOR LBAW2311 SCHEMA
+------------------------------------------------------------------------------------------------------------------------
 -- Create the User table (R01)
 CREATE TABLE member (
     user_id INT PRIMARY KEY,
@@ -41,7 +71,7 @@ CREATE TABLE member (
     user_birthdate TIMESTAMP NOT NULL,
     user_creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     user_score INT DEFAULT 0,
-	CONSTRAINT check_time CHECK (EXTRACT(YEAR FROM user_birthdate)- EXTRACT(YEAR FROM user_creation_date)=12 )
+	CONSTRAINT check_time CHECK (EXTRACT(YEAR FROM user_creation_date) - EXTRACT(YEAR FROM user_birthdate) > 12 )
 );
 
 -- Create the Tag table (R15)
@@ -94,12 +124,11 @@ CREATE TABLE notification (
     FOREIGN KEY (notification_user) REFERENCES member(user_id)
 );
 
-
 -- Create the Answer table (R13)
 CREATE TABLE answer (
     content_id INT PRIMARY KEY,
     question_id INT,
-        --Common to content
+    --Common to content
     content_author INT,
     content_creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     content_text VARCHAR(255) NOT NULL,
@@ -123,8 +152,6 @@ CREATE TABLE question (
     FOREIGN KEY (content_author) REFERENCES member(user_id)
 );
 
-
-
 -- Create a foreign key for question_tag in the question table
 ALTER TABLE question
     ADD CONSTRAINT FK_Tag_Question
@@ -135,11 +162,9 @@ ALTER TABLE question
     ADD CONSTRAINT FK_Correct_Answer
         FOREIGN KEY (correct_answer) REFERENCES answer(content_id);
 
-
 ALTER TABLE answer
     ADD CONSTRAINT fk_question
         FOREIGN KEY (question_id) REFERENCES question(content_id);
-
 
 -- Create the Comment table (R14)
 CREATE TABLE comment (
@@ -156,8 +181,6 @@ CREATE TABLE comment (
     FOREIGN KEY (answer_id) REFERENCES answer(content_id)
 );
 
-
-
 -- Create the Vote table (R16)
 CREATE TABLE vote (
     vote_id INT PRIMARY KEY,
@@ -165,11 +188,13 @@ CREATE TABLE vote (
     vote_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     upvote voteType NOT NULL,
     entity_voted entityType NOT NULL,
-    vote_content INT,
+    vote_content_question INT,
+    vote_content_answer INT,
+    vote_content_comment INT,
     FOREIGN KEY (vote_author) REFERENCES member(user_id),
-    FOREIGN KEY (vote_content) REFERENCES question(content_id),
-    FOREIGN KEY (vote_content) REFERENCES answer(content_id),
-    FOREIGN KEY (vote_content) REFERENCES comment(content_id)
+    FOREIGN KEY (vote_content_question) REFERENCES question(content_id),
+    FOREIGN KEY (vote_content_answer) REFERENCES answer(content_id),
+    FOREIGN KEY (vote_content_comment) REFERENCES comment(content_id)
 );
 
 -- Create the Report table (R17)
@@ -177,7 +202,9 @@ CREATE TABLE report (
     report_id INT PRIMARY KEY,
     report_creator INT,
     report_handler INT,
-    content_reported INT,
+    content_reported_question INT,
+    content_reported_answer INT,
+    content_reported_comment INT,
     report_reason reportReasonType NOT NULL,
     report_text VARCHAR(255),
     report_dealt BOOLEAN NOT NULL DEFAULT FALSE,
@@ -185,9 +212,9 @@ CREATE TABLE report (
     report_answer VARCHAR(255),
     FOREIGN KEY (report_creator) REFERENCES member(user_id),
     FOREIGN KEY (report_handler) REFERENCES moderator(user_id),
-    FOREIGN KEY (content_reported) REFERENCES question(content_id),
-    FOREIGN KEY (content_reported) REFERENCES answer(content_id),
-    FOREIGN KEY (content_reported) REFERENCES comment(content_id)
+    FOREIGN KEY (content_reported_question) REFERENCES question(content_id),
+    FOREIGN KEY (content_reported_answer) REFERENCES answer(content_id),
+    FOREIGN KEY (content_reported_comment) REFERENCES comment(content_id)
     --FOREIGN KEY (report_answer) REFERENCES answer(content_id)
 );
 
@@ -199,3 +226,169 @@ CREATE TABLE userfollowquestion (
     FOREIGN KEY (user_id) REFERENCES member(user_id),
     FOREIGN KEY (question_id) REFERENCES question(content_id)
 );
+
+
+
+
+
+
+
+------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------
+-- CREATE POPULATE FOR LBAW2311 SCHEMA
+------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------
+
+------------------------------------------------------------------------------------------------------------------------
+-- POPULATE TABLES FOR LBAW2311 SCHEMA
+------------------------------------------------------------------------------------------------------------------------
+-- Populate the User table (R01)
+--  The user_creation_date is automatically generated by the database
+INSERT INTO member (user_id, username, user_email, user_password, picture, user_birthdate, user_score)
+VALUES (1, 'admin', 'admin@example.com', 'pass', '/picture/avatar1.jpg', '1990-01-15', 4 ),
+       (2, 'moderator', 'moderator@example.com', 'pass', '/picture/avatar2.jpg', '1990-01-15',  1 ),
+       (3, 'member1', 'member1@example.com', 'pass','/picture/avatar3.jpg', '1990-01-21', 3 );
+
+
+
+-- Populate the Tag table (R15)
+INSERT INTO tag (tag_id, tag_name, tag_description)
+VALUES (1, 'programming', 'Programming-related questions'),
+       (2, 'database', 'Database management topics'),
+       (3, 'web', 'Web development questions'),
+       (4, 'networking', 'Networking-related questions'),
+       (5, 'security', 'Security-related questions'),
+       (6, 'hardware', 'Hardware-related questions'),
+       (7, 'software', 'Software-related questions'),
+       (8, 'mobile', 'Mobile development questions'),
+       (9, 'game', 'Game development questions'),
+       (10, 'other', 'Other questions');
+
+-- Populate the Admin table (R02)
+INSERT INTO admin (user_id)
+VALUES (1);
+
+-- Populate the Moderator table (R03)
+-- The assignment timestamp is automatically generated by the database
+INSERT INTO moderator (user_id, tag_id)
+VALUES (2, 1);
+
+-- Populate the Badge table (R04)
+INSERT INTO badge (badge_id, badge_name, badge_description)
+VALUES (1, 'Bronze', 'Bronze-level badge'), -- Assigned when you gained 10 points
+       (2, 'Silver', 'Silver-level badge'), -- Assigned when you gained 100 points
+       (3, 'Gold', 'Gold-level badge'), -- Assigned when you gained 1000 points
+       (4, 'Reliable', 'One of your answers was accepted as the correct answer'), -- Assigned when you have an answer accepted as the correct answer
+       (5, 'Notable Question', 'Your question was upvoted 100 times'), -- Assigned when you have a question with 100 upvotes
+       (6, 'Good Question', 'Your question was upvoted 25 times'), -- Assigned when you have a question with 25 upvotes
+       (7, 'Nice Question', 'Your question was upvoted 10 times'), -- Assigned when you have a question with 10 upvotes
+       (8, 'Notable Answer', 'Your answer was upvoted 100 times'), -- Assigned when you have an answer with 100 upvotes
+       (9, 'Good Answer', 'Your answer was upvoted 25 times'), -- Assigned when you have an answer with 25 upvotes
+       (10, 'Nice Answer', 'Your answer was upvoted 10 times'), -- Assigned when you have an answer with 10 upvotes
+       (11, 'Welcome', 'You have joined the community'); -- Assigned when you create your account
+
+-- Populate the UserBadge table (R05)
+-- The user_badge_date is automatically generated by the database
+INSERT INTO userbadge (userbadge_id, user_id, badge_id)
+VALUES (1, 1, 11),  -- Welcome badge for admin
+       (2, 2, 11),  -- Welcome badge for moderator
+       (3, 3, 11),  -- Welcome badge for member1
+       (4, 1, 4);   -- Reliable badge for admin
+
+
+
+-- NECESSARY TO COMPLETE AFTER CRETING THE CONTENTS AND UPVOTES IN ADITTION TO POINTS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+
+-- Populate the Notification table (R06)
+-- The notification_date is automatically generated by the database
+-- The notification_is_read is automatically set to false by the database
+--INSERT INTO notification (notification_id, notification_user, notification_content, notification_type)
+--VALUES (1, 1, 'You have a new message', 'message'),
+--       (2, 2, 'New answer to your question', 'answer'),
+--       (3, 3, 'New comment on your answer', 'comment'),
+--       (4, 1, 'You have a new badge', 'badge');
+
+
+-- NOTIFICATION IS INITIALLY EMPTY AS THESE ARE THE DEFAULTS FOR TEST PURPOSES
+-- AFTER THE FEATURES ARE IMPLEMENTED IN THE WEBSITE, THE NOTIFICATIONS WILL BE
+-- CRETED BY THE TRIGGERS AND POPULATED ON ITS OWN
+
+
+
+
+-- Populate the Question table (R12)
+-- The content_creation_date is automatically generated by the database
+-- The content_is_edited is automatically set to false by the database
+-- The content_is_visible is automatically set to true by the database
+INSERT INTO question (content_id, question_title, question_tag, correct_answer, content_author, content_text)
+VALUES (1, 'How to program in Python?', 1, NULL, 3, 'I want to learn Python programming. Any advice is helpful.'),             -- Question from member1
+       (2, 'Database design tips', 2, NULL, 2, 'I need advice on using docker with a database. Any help is appreciated.');  -- Question from moderator
+
+-- Populate the Answer table (R13)
+-- The content_creation_date is automatically generated by the database
+-- The content_is_edited is automatically set to false by the database
+-- The content_is_visible is automatically set to true by the database
+INSERT INTO answer (content_id, question_id, content_author, content_text)
+VALUES (3, 1, 1, 'Start with Python basics. Learn data types, variables, and control structures.'), -- Answer to question 'How to program in Python?' from admin
+       (4, 1, 2, 'Practice Python syntax by writing small programs. Use online resources.'),        -- Answer to question 'How to program in Python?' from moderator
+       (5, 1, 1, 'Explore Python libraries like NumPy and Pandas for data analysis.'),              -- Answer to question 'How to program in Python?' from admin
+       (6, 2, 1, 'Ensure a clear data model. Normalize tables for efficient data storage.'),        -- Answer to question 'Database design tips' from admin
+       (7, 2, 3, 'Use a database management system like MySQL or PostgreSQL.'),                     -- Answer to question 'Database design tips' from member1
+       (8, 2, 1, 'Consider indexes for faster queries. Plan for data backup and recovery.');        -- Answer to question 'Database design tips' from admin
+
+-- Insert correct answer into question 1
+UPDATE question
+SET correct_answer = 3
+WHERE content_id = 1;
+
+-- Populate the Comment table (R14)
+-- The content_creation_date is automatically generated by the database
+-- The content_is_edited is automatically set to false by the database
+-- The content_is_visible is automatically set to true by the database
+INSERT INTO comment (content_id, answer_id, content_author, content_text)
+VALUES (9, 3, 3, 'Great advice! Thanks for sharing.'), -- Comment on answer 3  of question 1 from member1
+       (10, 4, 3, 'Thanks for the information. But do you have anything more specific?'), -- Comment on answer 4 of question 1 from member1
+       (11, 4, 1, 'You''re welcome! Keep coding!'), -- Comment on answer 4 of question 1 from admin
+       (12, 6, 3, 'Normalization is a key concept in DB design.'), -- Comment on answer 6 of question 2 from member1
+       (13, 7, 1, 'Consider PostgreSQL for database management. It is what I use.'), -- Comment on answer 7 of question 2 from admin
+       (14, 8, 3, 'Data backup is crucial for disaster recovery.'); -- Comment on answer 8 of question 2 from member1
+
+-- Populate the Vote table (R16)
+-- The vote_date is automatically generated by the database
+INSERT INTO vote (vote_id, vote_author, upvote, entity_voted, vote_content_question, vote_content_answer, vote_content_comment)
+VALUES (1, 2, 'up', 'answer',NULL ,3, NULL ), -- Upvote answer 3 (belongs to admin) of question 1 from moderator
+       (2, 1, 'up', 'answer',NULL, 7, NULL), -- Upvote answer 7 (belongs to member1) of question 2 from admin
+       (3, 1, 'down', 'question', 2, NULL, NULL), -- Downvote question 2 (belongs to moderator) from admin
+       (4, 3, 'up', 'question', 2, NULL, NULL ), -- Upvote question 2 (belongs to moderator) from member1
+       (5, 3, 'up', 'answer', NULL , 3, NULL); -- Upvote answer 3 (belongs to admin) from member1
+
+--Points accumulated from votes
+-- Organized by vote_id
+-- 1 - admin - +1 - moderator - +1
+-- 2 - admin - +1 - member1 - +1
+-- 3 - admin - +1 - moderator - -1
+-- 4 - moderator - +1 - member1 - +1
+-- 5 - member1 - +1 - admin - +1
+-- Total points:
+-- admin - 4
+-- moderator - 1
+-- member1 - 3
+
+
+
+-- Populate the Report table (R17)
+-- The report_dealt is automatically set to false by the database
+INSERT INTO report (report_id, report_creator, report_handler, content_reported_question, 
+            content_reported_answer, content_reported_comment , report_reason, report_text)
+VALUES (1, 3, NULL, NULL, 4, NULL, 'spam', 'Inappropriate content'), -- Report on answer 4 from member1
+       (2, 1, NULL, 2, NULL, NULL, 'offensive', 'Offensive language'); -- Report on question 2 from admin
+
+-- THESE REPORTS ARE TO BE ASSIGNED A HANDLER AND BE REFUSED
+-- FOR TEST PURPOSES AFTER THE FEATURES ARE IMPLEMENTED
+
+-- Populate the UserFollowQuestion table (R18)
+INSERT INTO userfollowquestion (user_id, question_id, follow)
+VALUES (1, 1, true),  -- Follow question 1 from admin. He wants to be notified of any new activity
+       (2, 2, false), -- Unfollow question 2 from moderator. It's his own question but he does not want any more notifications about it
+       (3, 1, true),  -- Follow question 1 from member1. A user must follow his own questions by default
+       (3, 2, true);  -- Follow question 2 from member1. He wants to be notified of any new activity
