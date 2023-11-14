7903af37bb9a749095d0b3ee459d631c43730460
up201804832 <up201804832@fc.up.pt>
2023-11-14 14:20:16 +0000

    Question-page-view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
         <!-- Styles -->
         <link href="{{ url('css/milligram.min.css') }}" rel="stylesheet">
         <link href="{{ url('css/app.css') }}" rel="stylesheet">
+        <link href="{{ url('/css/question-page.css') }}" rel="stylesheet">
         <script type="text/javascript">
             // Fix for Firefox autofocus CSS bug
             // See: http://stackoverflow.com/questions/18943276/html-5-autofocus-messes-up-css-loading/18945951#18945951
--- resources/views/pages/question.blade.php
+++ resources/views/pages/question.blade.php
@@ -1,22 +1,157 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <strong>Tag:</strong> {{ $question->tag->tag_name }}
-        <h1>{{ $question->question_title }}</h1>
-        <h2>{{ $question->content_text }}</h2>
-        <div>
-            <strong>Author:</strong> {{ $question->author->username }}
-        </div>
-        <div>
-            <br><h3>{{ $question->answer_count }} Answers: </h3>
-            @foreach ($question->answers as $answer)
-                <div>
-                    <p>{{$answer->author->username }} :</p>  
-                    <p>{{$answer->content_text }}</p> 
-                </div>
-            @endforeach    
-        </div>
-    </div>
 
+    <div class="container">
+        <div class="content_container"> <!--Question-->
+            <div class="content_top_container">
+
+                <div class="content_left_container">
+                    <div class="content_user_profile_photo">
+                        <img src="" alt="Profile Photo">
+                    </div>
+                    <p><b>{{ $question->author->username }}</b></p>
+                </div>
+                
+                <div class="question_tittle_container">
+                    <p>
+                        <strong>Tag:</strong> {{ $question->tag->tag_name }}
+                    </p>
+                    <h1>{{ $question->question_title }}</h1>
+                    <p>
+                        <strong>Created at: </strong>{{$question->content_creation_date}}
+                    </p>
+                </div>
+
+                @if(true) <!-- TODO: restrict access only for owner -->
+                <div class="content_right_container"> 
+                    <button> 
+                        delete
+                    </button>
+                    <button> 
+                        Edit
+                    </button>
+                </div>
+                @endif
+            </div>
+
+            <div class="content_bottom_container">
+
+                <div class="content_left_container">
+
+                </div>
+                
+                <div class="content_text_container">
+                    <h3>{{ $question->content_text }}</h3>
+                    @if($question->content_is_edited)
+                    <p>edited</p>
+                    @endif
+                </div>
+
+                <div class="content_right_container"> 
+                    <button> 
+                        LIKE
+                    </button>
+                    <button> 
+                        DISLIKE
+                    </button>
+                    <p><b>999</b></p>
+                </div>
+            </div>
+        </div>
+
+
+        
+        @if($question->answer_count !== 1)
+            <br><h3>{{ $question->answer_count }} Answers: </h3>
+        @else
+            <br><h3>{{ $question->answer_count }} Answer: </h3>
+        @endif
+
+
+        @foreach ($question->answers as $answer)
+        <div class="content_container">
+            <div class="content_top_container">
+
+                <div class="content_left_container">
+                    <div class="content_user_profile_photo">
+                        <img src="" alt="Profile Photo">
+                    </div>
+                    <p><b>{{$answer->author->username }}</b></p>
+                </div>
+                
+                <div class="content_text_container">
+                    <p>
+                        <h3>{{ $answer->content_text }}</h3>
+                    </p>
+                    <p>
+                        <strong>Created at: </strong>{{$answer->content_creation_date}}
+                    </p>
+                </div>
+
+                @if(true) <!-- TODO: restrict access only for owner -->
+                <div class="content_right_container"> 
+                    <button> 
+                        delete
+                    </button>
+                    <button> 
+                        Edit
+                    </button>
+                    <button> 
+                        LIKE
+                    </button>
+                    <button> 
+                        DISLIKE
+                    </button>
+                    <p><b>88</b></p>
+                </div>
+                @endif
+            </div>
+        </div>
+
+        @foreach ($answer->comments as $comment)
+        <p>{{$comment->content_text}}</p>
+        <div class="comment_container">
+            <div class="content_top_container">
+
+                <div class="content_left_container">
+                    <div class="content_user_profile_photo">
+                        <img src="" alt="Profile Photo">
+                    </div>
+                    <p><b>{{$comment->author->username }}</b></p>
+                </div>
+                
+                <div class="content_text_container">
+                    <p>
+                        <h4>Comment : {{ $comment->content_text }}</h4>
+                    </p>
+                    <p>
+                        <strong>Created at: </strong>{{$comment->content_creation_date}}
+                    </p>
+                </div>
+
+                @if(true) <!-- TODO: restrict access only for owner -->
+                <div class="content_right_container"> 
+                    <button> 
+                        delete
+                    </button>
+                    <button> 
+                        Edit
+                    </button>
+                    <button> 
+                        LIKE
+                    </button>
+                    <button> 
+                        DISLIKE
+                    </button>
+                    <p><b>77</b></p>
+                </div>
+                @endif
+            </div>
+        </div>
+        @endforeach
+        <hr>
+        @endforeach    
+
+    </div>
 @endsection
