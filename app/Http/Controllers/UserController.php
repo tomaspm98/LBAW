c87702c130a395daf617ba75de85ff5c1d3d7b9a
Gonçalo Nuno Leitão Pinho da Costa <up202103336@up.pt>
2023-11-16 08:49:30 +0000

    alter birthdate working

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,6 +10,8 @@ use Illuminate\Support\Facades\Validator;
 use Carbon\Carbon;
 
 
+use Illuminate\Validation\Rule;
+
 
 class UserController extends Controller
 {
@@ -38,12 +40,36 @@ class UserController extends Controller
             'username' => 'nullable|string|max:255|unique:member,username,' . $user_id . ',user_id',
             'user_email' => 'nullable|email|unique:member,user_email,' . $user_id . ',user_email',
             'user_password' => 'nullable|string|max:255|confirmed',
-            'user_birthdate' => 'nullable|date',
+            'user_birthdate' => 'nullable|date|before_or_equal:' . now()->subYears(12)->format('Y-m-d'), Carbon::parse($request->user_birthdate)->toDateTimeString()
+        ],
+
+        [ // Custom error messages
+            'username.string' => 'The username must be a string.',
+            'username.max' => 'The username must not exceed 255 characters.',
+            'username.unique' => 'The username is already taken.',
+            
+            'user_email.string' => 'The email must be a string.',
+            'user_email.email' => 'The email must be a valid email address.',
+            'user_email.max' => 'The email must not exceed 255 characters.',
+            'user_email.unique' => 'The email is already taken.',
+            
+            'password.string' => 'The password must be a string.',
+            'password.min' => 'The password must be at least 8 characters.',
+            'password.confirmed' => 'The password confirmation does not match.',
+            
+            'picture.image' => 'The uploaded file must be an image.',
+            'picture.mimes' => 'Only PNG, JPEG, and SVG formats are allowed.',
+            'picture.max' => 'The file size must not exceed 10 megabytes.',
+
+            'user_birthdate.date' => 'The birthdate must be a valid date.',
+            'user_birthdate.before_or_equal' => 'The birthdate must be at least 12 years ago.',
         ]);
+
 
         $member = Member::findOrFail($user_id);        
         $attributes = array_filter($request->all());
         $member->update($attributes);
+
 
         return redirect()->route('member.show', ['user_id' => $user_id])->with('success', 'User updated successfully');
     }
