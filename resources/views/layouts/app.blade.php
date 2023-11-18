7375bee82f6474d6abd0590d9bf4ea11084f949c
up201804832 <up201804832@fc.up.pt>
2023-11-18 13:42:36 +0000

    Home-page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,7 @@
         <link href="{{ url('css/app.css') }}" rel="stylesheet">
         <link href="{{ url('css/navbar.css') }}" rel="stylesheet">
         <link href="{{ url('/css/question-page.css') }}" rel="stylesheet">
+        <link href="{{ url('/css/home-page.css') }}" rel="stylesheet">
         <script type="text/javascript">
             // Fix for Firefox autofocus CSS bug
             // See: http://stackoverflow.com/questions/18943276/html-5-autofocus-messes-up-css-loading/18945951#18945951
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,13 +9,36 @@
 <div class="container">
 
     <h2>Top Questions</h2>
-    listQuestions
-
     <ul>
         @foreach ($questions as $question)
-            <li>
-                <a href="{{ route('questions.show', $question->question_id) }}">{{ $question->question_title }}</a>
-                - <small>Asked by {{ $question->author->username ?? 'Unknown' }}</small>
+            <li class="question_card">
+                
+                <!-- <small>Asked by {{ $question->author->username ?? 'Unknown' }}</small> -->
+
+                <div class="question_user_container">
+                    <a href=""> <!-- route('member.show', $question->author) -->
+                        <div class="question_user_photo">
+                            <img src="" alt="Profile Photo">
+                        </div>
+                    </a>
+                    <p><b>{{ $question->author->username ?? 'Unknown' }}</b></p>
+                </div>
+                
+                <div class="top_questions_tittle">
+                    <a href="{{ route('questions.show', $question->question_id) }}"> <h3>{{ $question->question_title }}</h3></a>     
+                    <a class="more_details" href="{{ route('questions.show', $question->question_id) }}"> more details</a>           
+                </div>
+                <div class="top_questions_n_answers">
+                    @if($question->answer_count !== 1)
+                    <p>{{$question->answer_count}} answers</p>
+                    @else
+                    p>{{$question->answer_count}} answer</p>
+                    @endif
+                </div>
+
+                <div class="top_questions_votes">
+                    <p>{{$question->vote_count}} votes</p> 
+                </div>
             </li>
         @endforeach
     </ul>
