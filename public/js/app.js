d4319a2893cde09e09cc7cea0f915bbde653724d
ricardo-peralta-0000 <ricardoperalta0000@gmail.com>
2023-11-21 00:58:29 +0000

    Thingy removed

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-function addEventListeners() {
+/*function addEventListeners() {
     let itemCheckers = document.querySelectorAll('article.card li.item input[type=checkbox]');
     [].forEach.call(itemCheckers, function(checker) {
       checker.addEventListener('change', sendItemUpdateRequest);
@@ -178,7 +178,7 @@ function addEventListeners() {
   }
   
   addEventListeners();
-  
+  */
 
 
   function updateQuestionCount() {
